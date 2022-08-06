a92503bc7308945246e50c5f8d070d526dfeeb28
Jose Anania <joseanania47@gmail.com>
2022-08-06 12:29:06 -0300

    commit 10

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
 
-  // creamos la base de la URL (igual en todas las peticiones HTTP)
-  base_url: 'http://localhost:3000/api'
+  // creamos la base de la URL (igual en todas las peticiones HTTP) 
+  // copiamos el URL de Producción desde Heroku
+  base_url: 'https://adminpro-backend-jla.herokuapp.com/api'
 };
